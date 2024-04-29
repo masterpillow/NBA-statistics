8c69b27b66b79339487216d3b86c3f064e0dcdea
Max Musial <144265654+masterpillow@users.noreply.github.com>
2024-04-29 10:25:47 -0400

    Update streamlit-NBA.py

--- streamlit-NBA.py
+++ streamlit-NBA.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
 
 def load_data():
     # Load the data from a CSV file
